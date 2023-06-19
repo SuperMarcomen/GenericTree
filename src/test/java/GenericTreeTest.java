ba4241a55fa1d7e103acab003146fad3860dd36f
SuperMarcomen <marcodemartino23@gmail.com>
2023-06-19 23:47:03 +0200

    I even pass this weird chinese stuff provided from uni, wow!

--- src/test/java/GenericTreeTest.java
+++ src/test/java/GenericTreeTest.java
@@ -4,50 +4,74 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class GenericTreeTest {
 
-    private static GenericTree<Integer> genericTree = new GenericTree<>(5);
+    private static final GenericTree<Integer> GENERIC_TREE = new GenericTree<>(5);
 
     @BeforeAll
     static void setup() {
-        genericTree.add(4, 5);
-        genericTree.add(3, 5);
-        genericTree.add(2, 3);
-        genericTree.add(1, 2);
-        genericTree.add(0, 2);
-        genericTree.add(null, 4);
+        GENERIC_TREE.add(4, 5);
+        GENERIC_TREE.add(3, 5);
+        GENERIC_TREE.add(2, 3);
+        GENERIC_TREE.add(1, 2);
+        GENERIC_TREE.add(0, 2);
+        GENERIC_TREE.add(null, 4);
+    }
+
+    @Test
+    void chineseStuff() {
+        GenericTree<String> treeOperations = new GenericTree<>("你好");
+        treeOperations.add("很高兴认识你", "你好");
+        assertThrows(NoSuchElementException.class, () -> treeOperations.removeSubtree("你号"));
+
+        assertEquals(List.of("你好", "很高兴认识你"), treeOperations.removeSubtree("你好"));
+        assertThrows(NoSuchElementException.class, () -> treeOperations.removeSubtree("很高兴认识你"));
+        assertThrows(NoSuchElementException.class, () -> treeOperations.getRoot());
+
+        assertEquals(0, treeOperations.size());
+        assertEquals(-1, treeOperations.height());
+
+
+        GenericTree<String> treeOperations2 = new GenericTree<>("你好");
+        treeOperations2.add("你会中文吗?", "你好");
+        treeOperations2.add("你好，我会一点点中文", "你会中文吗?");
+        treeOperations2.add("Sorry mate, I don't speak chinese", "你会中文吗?");
+        treeOperations2.add("太好了， 你的大学很好", "你好，我会一点点中文");
+        List<String> expected = List.of("你会中文吗?", "你好，我会一点点中文", "Sorry mate, I don't speak chinese", "太好了， 你的大学很好");
+        assertEquals(expected, treeOperations2.removeSubtree("你会中文吗?"));
     }
 
     @Test
     @Order(1)
     void getRoot() {
-        assertEquals(5, genericTree.getRoot());
+        assertEquals(5, GENERIC_TREE.getRoot());
     }
 
     @Test
     @Order(2)
     void getChildren() {
         List<Integer> children = List.of(4, 3);
-        assertEquals(children, genericTree.getChildren(5));
+        assertEquals(children, GENERIC_TREE.getChildren(5));
 
         children = List.of(2);
-        assertEquals(children, genericTree.getChildren(3));
+        assertEquals(children, GENERIC_TREE.getChildren(3));
 
-        assertEquals(Collections.emptyList(), genericTree.getChildren(null));
+        assertEquals(Collections.emptyList(), GENERIC_TREE.getChildren(null));
     }
 
     @Test
     @Order(3)
     void getParent() {
-        assertEquals(5, genericTree.getParent(4));
-        assertEquals(3, genericTree.getParent(2));
+        assertEquals(5, GENERIC_TREE.getParent(4));
+        assertEquals(3, GENERIC_TREE.getParent(2));
     }
 
     @Test
@@ -58,26 +82,26 @@ class GenericTreeTest {
     @Test
     @Order(5)
     void size() {
-        assertEquals(7, genericTree.size());
+        assertEquals(7, GENERIC_TREE.size());
     }
 
     @Test
     @Order(6)
     void removeLeaf() {
-        genericTree.removeLeaf(null);
-        assertEquals(6, genericTree.size());
+        GENERIC_TREE.removeLeaf(null);
+        assertEquals(6, GENERIC_TREE.size());
     }
 
     @Test
     @Order(7)
     void height() {
-        assertEquals(4, genericTree.height());
+        assertEquals(4, GENERIC_TREE.height());
     }
 
     @Test
     @Order(8)
     void removeSubtree() {
         List<Integer> nodes = List.of(3, 2, 1, 0);
-        assertEquals(nodes, genericTree.removeSubtree(3));
+        assertEquals(nodes, GENERIC_TREE.removeSubtree(3));
     }
 }
